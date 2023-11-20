c7ed12b7dcd22b61da4efedab03e1d27536b5804
Carlos Moreno <carlooos_28@hotmail.com>
2023-11-19 21:32:43 -0500

    Fix bug password

--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,16 @@
 ActiveRecord::Schema[7.1].define(version: 2023_11_20_003921) do
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email"
-    t.string "password_digest"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "role"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
